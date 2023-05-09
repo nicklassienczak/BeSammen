ba2ed6f23a25a810f6b3bc24b09b2151ea42a979
arnefogh <andreasfogh7@gmail.com>
2023-05-09 13:43:39 +0200

    Rebased the develop branch with the master

--- app/src/main/java/com/example/besammen/MainActivity.java
+++ app/src/main/java/com/example/besammen/MainActivity.java
@@ -36,5 +36,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        mforgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ForgotPassword.class));
+            }
+        });
+
     }
 }
