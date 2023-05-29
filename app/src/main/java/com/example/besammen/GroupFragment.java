6e36e59fa75626658dd849ac7f151910a5d367ad
arnefogh <andreasfogh7@gmail.com>
2023-05-29 12:17:20 +0200

    Added a group chat function, all should work, just need to test if the receiving users name will show on the message

--- app/src/main/java/com/example/besammen/GroupFragment.java
+++ app/src/main/java/com/example/besammen/GroupFragment.java
@@ -1,20 +1,261 @@
 package com.example.besammen;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.squareup.picasso.Picasso;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
 public class GroupFragment extends Fragment {
+
+    private EditText mgetMessageGroup;
+    private TextView msendMessageGroup;
+
+    //RecyclerView to show the messages
+    private RecyclerView mrecyclerViewGroupChat;
+
+    //This is used so store the text the user puts in the edittext before it gets sent to the sender chat layout
+    private String inputMessageGroup, mnameOfReceivingUser, mnameOfSenderUser;
+
+    //We need a intent because we are getting the data from our chat fragment
+    Intent intent;
+
+    //We need to be able to store the name and Uid of the receiver and the sender
+    private String mreceiverName, mreceiverUid, msenderUid;
+
+    //We need a string to hold the current time
+    private String curretTime;
+    private Calendar calendar;
+    //To format the date
+    private SimpleDateFormat simpleDateFormat;
+
+    //We need to create a room for sender and receiver, because when working with database we need to store them seperately like this
+    private String senderRoom, receiverRoom;
+
+    private FirebaseAuth firebaseAuth;
+    private FirebaseDatabase firebaseDatabase;
+    private FirebaseFirestore firebaseFirestore;
+
+    private MessagesAdapterGroup messagesAdapterGroup;
+    private ArrayList<MessagesGroup> messagesArrayList;
 
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.groupfragment, null);
+        //So we can find the elements inside chatFragment.java
+        View viewGROUP = inflater.inflate(R.layout.groupfragment, container, false);
+
+        mgetMessageGroup = viewGROUP.findViewById(R.id.getMessageGroup);
+        msendMessageGroup = viewGROUP.findViewById(R.id.sendMessageGroup);
+        mrecyclerViewGroupChat = viewGROUP.findViewById(R.id.recyclerViewGroupChat);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+
+        calendar = Calendar.getInstance();
+        //This is to change the format so that it is in hours and minuts, and the "a" is for 24 hour format
+        simpleDateFormat = new SimpleDateFormat("hh:mm a");
+
+        //Who is the sender, sender Uid
+        //This gets the Uid of the sender and stores it
+        msenderUid = firebaseAuth.getUid();
+        mreceiverUid = firebaseFirestore.collection("Users").getId();
+
+
+
+        //Who is the receiver, receiving Uid
+        //We are getting that Uid from the chat fragment
+        //mreceiverUid = getIntent().getStringExtra("receiverUid");
+        //Getting the receiver name from chat fragment
+        //mreceiverName = getIntent().getStringExtra("name");
+
+        //To take the data from the intent
+        //intent = getIntent();
+
+
+        String currentUserUid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+
+        firebaseFirestore.collection("Users")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()){
+                            List<String> allUsersUid = new ArrayList<>();
+                            List<String> allUsersNames = new ArrayList<>();
+
+                            for (QueryDocumentSnapshot document : task.getResult()){
+                                String userUid = document.getId();
+                                String userName = document.getString("name");
+                                if (!userUid.equals(currentUserUid)){
+                                    allUsersUid.add(userUid);
+                                    allUsersNames.add(userName);
+                                }
+                                if (userUid.equals(msenderUid)){
+                                    mnameOfSenderUser = userName;
+                                }
+                                if (userUid.equals(mnameOfReceivingUser)){
+                                    mnameOfReceivingUser = userName;
+                                }
+                            }
+                            senderRoom = currentUserUid + allUsersUid.get(0);
+                            receiverRoom = allUsersUid.get(0) + currentUserUid;
+
+
+
+
+
+                            //The array list of all the messages
+                            messagesArrayList = new ArrayList<>();
+
+                            //To store all the messages inside the realtime database
+                            DatabaseReference databaseReference = firebaseDatabase.getReference().child("groupChats").child(senderRoom).child("groupMessages");
+                            messagesAdapterGroup = new MessagesAdapterGroup(getActivity(), messagesArrayList);
+                            databaseReference.addValueEventListener(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                    //Now we have to clear the previous arraylist
+                                    messagesArrayList.clear();
+                                    for (DataSnapshot snapshot1 : snapshot.getChildren()){
+                                        MessagesGroup messages = snapshot1.getValue(MessagesGroup.class);
+                                        messagesArrayList.add(messages);
+                                    }
+                                    messagesAdapterGroup.notifyDataSetChanged();
+
+
+
+                                }
+
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError error) {
+
+                                }
+                            });
+
+
+                            LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+                            //This is because we want to start recyclerview from reverse
+                            linearLayoutManager.setStackFromEnd(true);
+                            mrecyclerViewGroupChat.setLayoutManager(linearLayoutManager);
+                            messagesAdapterGroup = new MessagesAdapterGroup(getActivity(), messagesArrayList);
+                            mrecyclerViewGroupChat.setAdapter(messagesAdapterGroup);
+
+                        }
+                        else {
+                            Toast.makeText(getContext(), "Kunne ikke finde brugeren", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+
+
+
+
+        //To change the name in the toolbar of the specific receiving user, we have to parse the name that is stored in the mreceivername
+
+
+        //If the user clicks on the send text, the message will be sent
+        msendMessageGroup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //Then we will take the message that we have stored in the input message
+                inputMessageGroup = mgetMessageGroup.getText().toString();
+                if (inputMessageGroup.isEmpty()){
+                    //We will check if the user even have entered a message by checking if it's empty.
+                    //If it is, this message will show
+                    //Toast.makeText(getApplicationContext(), "Skriv venligst en besked først", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    //This is date for timestamp
+                    Date date = new Date();
+                    //Currenttime will now store the current time in the specific time format that i have declared in the simple date format
+                    curretTime = simpleDateFormat.format(calendar.getTime());
+
+                    //Now we have to send this message
+                    //This will create a new message through the message class, and all the necessary info is being parsed into that
+                    MessagesGroup messages = new MessagesGroup(inputMessageGroup, firebaseAuth.getUid(), curretTime, mnameOfSenderUser, mnameOfReceivingUser, date.getTime());
+
+                    //Now we have to store that on our database
+                    firebaseDatabase = FirebaseDatabase.getInstance();
+                    firebaseDatabase.getReference().child("groupChats")
+                            //Here we have to create a room for the sender, which contain the sender name and sender Uid
+                            .child(senderRoom)
+                            .child("groupMessages")
+                            .push().setValue(messages).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                @Override
+                                public void onComplete(@NonNull Task<Void> task) {
+                                    //If this happens successfully then we have to send this to the receiver though the receiverroom
+                                    firebaseDatabase.getReference()
+                                            .child("groupChats")
+                                            .child(receiverRoom)
+                                            .child("groupMessages")
+                                            .push().setValue(messages).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                @Override
+                                                public void onComplete(@NonNull Task<Void> task) {
+                                                    //Here i can display a toast to make sure the message has been sent
+                                                    System.out.println("Beskeden er blevet sendt");
+
+
+                                                    messagesAdapterGroup.notifyDataSetChanged();
+                                                }
+                                            });
+                                }
+                            });
+
+                    //After the message have been sent, then we will clear the edittext
+                    mgetMessageGroup.setText(null);
+
+
+                }
+
+
+            }
+        });
+
+
+        return viewGROUP;
+
+
+
     }
+
 }
---
+++ app/src/main/java/com/example/besammen/MessagesAdapterGroup.java
@@ -0,0 +1,133 @@
+package com.example.besammen;
+
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.ArrayList;
+
+public class MessagesAdapterGroup extends RecyclerView.Adapter {
+
+    private Context context;
+    private ArrayList<MessagesGroup> messagesArrayList;
+
+    //This will help identify if the user is sending a message or not
+    private int ITEM_SEND_GROUP = 1;
+    private int ITEM_RECEIVE = 2;
+
+    public MessagesAdapterGroup(Context context, ArrayList<MessagesGroup> messagesArrayList) {
+        this.context = context;
+        this.messagesArrayList = messagesArrayList;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        //If view type is equal to ITEM_SEND_GROUP this means that i am the sender
+        if (viewType == ITEM_SEND_GROUP){
+
+            //Then we have to attach the context of the message to the sender chat layout
+            View view = LayoutInflater.from(context).inflate(R.layout.sender_group_chat_layout, parent, false);
+            //This will bind the text to the sender view holder
+            return new SenderViewHolderGROUP(view);
+        }
+        else {
+
+            View view = LayoutInflater.from(context).inflate(R.layout.receiver_group_chat_layout, parent, false);
+
+            return new ReceiverViewHolderGROUP(view);
+
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+        //If the position equals the sender
+        MessagesGroup messages = messagesArrayList.get(position);
+        if (holder.getClass() == SenderViewHolderGROUP.class){
+            //The sender view holder will receive this information
+
+            SenderViewHolderGROUP senderViewHolder = (SenderViewHolderGROUP)holder;
+            senderViewHolder.msenderMessage.setText(messages.getMessage());
+            senderViewHolder.mtimeMessageSent.setText(messages.getCurrentTime());
+            senderViewHolder.msenderNameOnMessage.setText(messages.getSenderName());
+        }
+        //If its not the sender, then it's the receiver
+        else {
+            ReceiverViewHolderGROUP receiverViewHolder = (ReceiverViewHolderGROUP)holder;
+            receiverViewHolder.mreceiverMessage.setText(messages.getMessage());
+            receiverViewHolder.mtimeMessageSent.setText(messages.getCurrentTime());
+            receiverViewHolder.mreceiverNameOnMessage.setText(messages.getReceiverName());
+        }
+    }
+
+    //This will identify who the sender is
+    @Override
+    public int getItemViewType(int position) {
+        MessagesGroup messages = messagesArrayList.get(position);
+        //If the current users id is euqual to the sender id then is the sender
+        if (FirebaseAuth.getInstance().getCurrentUser().getUid().equals(messages.getSenderId())){
+            return ITEM_SEND_GROUP;
+        }
+
+        //If not it is the receiver
+        else {
+            return ITEM_RECEIVE;
+        }
+
+
+    }
+
+    //This will allow there to be an unlimited amount of messages
+    @Override
+    public int getItemCount() {
+        if (messagesArrayList != null) {
+            return messagesArrayList.size();
+        } else {
+            return 0;
+        }
+    }
+
+
+    //We have two layouts so thats why we need two view holder
+
+    class SenderViewHolderGROUP extends RecyclerView.ViewHolder{
+
+        //We have two things we need to show in our holder
+        TextView msenderMessage, mtimeMessageSent, msenderNameOnMessage;
+
+
+        public SenderViewHolderGROUP(@NonNull View itemView) {
+            super(itemView);
+            msenderMessage = itemView.findViewById(R.id.senderMessage);
+            mtimeMessageSent = itemView.findViewById(R.id.timeMessageSent);
+            msenderNameOnMessage = itemView.findViewById(R.id.senderNameOnMessage);
+
+        }
+    }
+
+    class ReceiverViewHolderGROUP extends RecyclerView.ViewHolder{
+
+        //We have two things we need to show in our holder
+        TextView mreceiverMessage, mtimeMessageSent, mreceiverNameOnMessage;
+
+
+        public ReceiverViewHolderGROUP(@NonNull View itemView) {
+            super(itemView);
+            mreceiverMessage = itemView.findViewById(R.id.receiverMessage);
+            mtimeMessageSent = itemView.findViewById(R.id.timeMessageSent);
+            mreceiverNameOnMessage = itemView.findViewById(R.id.receiverNameOnMessage);
+
+        }
+    }
+
+}
