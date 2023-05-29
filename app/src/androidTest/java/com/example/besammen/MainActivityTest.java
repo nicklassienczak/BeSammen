184e2ac7f51573fef4f96b26f16d2739ae47195d
nicklassienczak <nicklas-sk@hotmail.com>
2023-05-29 14:30:28 +0200

    Added test back to the repo

---
+++ app/src/androidTest/java/com/example/besammen/MainActivityTest.java
@@ -0,0 +1,118 @@
+package com.example.besammen;
+
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.pressImeActionButton;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityScenarioRule<MainActivity> mActivityScenarioRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void mainActivityTest() {
+        ViewInteraction relativeLayout = onView(
+                allOf(withId(com.hbb20.R.id.rlClickConsumer),
+                        childAtPosition(
+                                allOf(withId(com.hbb20.R.id.countryCodeHolder),
+                                        childAtPosition(
+                                                withId(R.id.countryCodePicker),
+                                                0)),
+                                0),
+                        isDisplayed()));
+        relativeLayout.perform(click());
+
+        ViewInteraction recyclerView = onView(
+                allOf(withId(com.hbb20.R.id.recycler_countryDialog),
+                        childAtPosition(
+                                withClassName(is("android.widget.RelativeLayout")),
+                                2)));
+        recyclerView.perform(actionOnItemAtPosition(53, click()));
+
+        ViewInteraction appCompatEditText = onView(
+                allOf(withId(R.id.getPhoneNumber),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                4),
+                        isDisplayed()));
+        appCompatEditText.perform(replaceText("26112394"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText2 = onView(
+                allOf(withId(R.id.getPhoneNumber), withText("26112394"),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                4),
+                        isDisplayed()));
+        appCompatEditText2.perform(pressImeActionButton());
+
+        ViewInteraction relativeLayout2 = onView(
+                allOf(withId(R.id.sendSmsAuth),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(android.R.id.content),
+                                        0),
+                                5),
+                        isDisplayed()));
+        relativeLayout2.perform(click());
+
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.smsAuthText), withText("Der sendes en sms for at bekr�fte din identitet"),
+                        withParent(withParent(withId(android.R.id.content))),
+                        isDisplayed()));
+        textView.check(matches(withText("Der sendes en sms for at bekr�fte din identitet")));
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
